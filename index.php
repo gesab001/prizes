933058add6db2f145d474c89719c99608d7cb46d
Giovanni Saberon <144000giovanni@gmail.com>
2020-04-26 13:53:07 +1200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body>
+<form action="/cgi-bin/addprize.py" method="post">
+<input type="text" name="name" placeholder="name">
+<br><br>
+<input type="text" name="url" placeholder="url">
+<br><br>
+<input type="submit" value="submit">
+
+</form>
+</body>
+</html>
